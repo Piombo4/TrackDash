b63100cd0e53807223ccbcd50c6002763bba1b0a
Piombo4 <apiombo6@gmail.com>
2024-03-14 14:29:39 +0100

    d

--- trackdash/lib/pages/StartActivity.dart
+++ trackdash/lib/pages/StartActivity.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class StartActivity extends StatefulWidget {
   const StartActivity({Key? key}) : super(key: key);
@@ -7,9 +8,23 @@ class StartActivity extends StatefulWidget {
   State<StartActivity> createState() => _StartActivityState();
 }
 
-class _StartActivityState extends State<StartActivity> {
+class _StartActivityState extends State<StartActivity>
+    with AutomaticKeepAliveClientMixin {
+  late MapController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = MapController.withUserPosition(
+        trackUserLocation: UserTrackingOption(
+      enableTracking: true,
+      unFollowUser: false,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.symmetric(vertical: 50),
@@ -18,7 +33,44 @@ class _StartActivityState extends State<StartActivity> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  width: 500,
+                  height: 550,
+                  child: OSMFlutter(
+                    controller: controller,
+                    osmOption: OSMOption(
+                      markerOption: MarkerOption(
+                        defaultMarker: MarkerIcon(
+                          icon: Icon(
+                            Icons.person_pin_circle,
+                            color: Colors.blue,
+                            size: 56,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Flexible(
+                child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  elevation: 8,
+                  minimumSize: Size(400, 60),
+                  backgroundColor: Colors.greenAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+              onPressed: () {},
+              child: Text(
+                "Start Run",
+                style: TextStyle(fontSize: 35, color: Colors.white),
+              ),
+            ))
+            /*Text(
               "TrackDash",
               style: TextStyle(fontSize: 30),
             ),
@@ -34,10 +86,13 @@ class _StartActivityState extends State<StartActivity> {
                 "Start Run",
                 style: TextStyle(fontSize: 40, color: Colors.white),
               ),
-            )
+            )*/
           ],
         ),
       ),
     ));
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
